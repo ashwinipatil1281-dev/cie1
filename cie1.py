0586e758a9553ad844b5ec7ad14637395bb8ce8e
ashwinipatil1281@gmail.com <ashwinipatil1281-dev>
2025-11-29 10:38:07 +0530

    save

---
+++ cie1.py
@@ -0,0 +1,15 @@
+import sys
+if len(sys.argv)<4
+   car number=sys.argv[1]
+   owner name=sys.argv[2]
+   vehicle type=sys.argv[3]
+   RC expiry year=sys.argv[4]
+else:
+   car number=9876
+   owner name=abc
+   vehicle type=BMW
+   RC expiry year=2030
+   print("car number:",number)
+   print("owner name:",name)
+   print("vehicle type")
+   print("RC expiry year:",year)

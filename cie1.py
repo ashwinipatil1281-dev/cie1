c62df65fa3565fc051f2fda4eb1636e385521c4a
ashwinipatil1281-dev <ashwinipatil1281@gmail.com>
2025-11-29 10:47:33 +0530

    Update cie1.py

--- cie1.py
+++ cie1.py
@@ -1,15 +1,15 @@
 import sys
 if len(sys.argv)<4:
-   car number=sys.argv[1]
-   owner name=sys.argv[2]
-   vehicle type=sys.argv[3]
-   RC expiry year=sys.argv[4]
+   car_number=sys.argv[1]
+   owner_name=sys.argv[2]
+   vehicle_type=sys.argv[3]
+   RC_expiry_year=sys.argv[4]
 else:
-   car number=9876
-   owner name=abc
-   vehicle type=BMW
-   RC expiry year=2030
-   print("car number:",number)
-   print("owner name:",name)
-   print("vehicle type")
-   print("RC expiry year:",year)
+   car_number=9876
+   owner_name=abc
+   vehicle_type=BMW
+   RC_expiry_year=2030
+print("car number:",number)
+print("owner name:",name)
+print("vehicle type:",type)
+print("RC expiry year:",year)
